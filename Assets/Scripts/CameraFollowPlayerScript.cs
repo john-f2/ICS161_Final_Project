a4f69633a8e14517d989ee32c10ec6f5565f0d41
arianazuko <ashansen@uci.edu>
2019-02-20 00:35:10 -0800

    Added Sprites, CameraFollowingPlayer Script, and SeasonChangeButton
    I drew the sprites quickly so we can change them whenever.

---
+++ Assets/Scripts/CameraFollowPlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayerScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player_position = player.transform.position;
+        this.transform.position = new Vector3(player_position.x, player_position.y, -10);
+    }
+}
---
+++ Assets/Scripts/SeasonChangeButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeasonChangeButtonScript : MonoBehaviour
+{
+    private bool isWinter = true;
+
+    [SerializeField] protected Sprite winter_sprite;
+    [SerializeField] protected Sprite summer_sprite;
+
+    private Image button_image;
+
+    void Awake()
+    {
+        button_image = this.GetComponent<Image>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSeasonChangeButtonClick()
+    {
+        isWinter = !isWinter;
+
+        if (isWinter)
+        {
+            button_image.sprite = winter_sprite;
+        }
+        else
+        {
+            button_image.sprite = summer_sprite;
+        }
+    }
+}
