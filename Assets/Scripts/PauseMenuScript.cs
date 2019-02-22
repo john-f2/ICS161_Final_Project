4c75a900f088129ae44031255471714125ea812f
arianazuko <ashansen@uci.edu>
2019-02-22 13:29:42 -0800

    Imported TMPro, PlayerInventory for Prototype Puzzle, and Pause Menu with Quit

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public bool isPaused = false;
+    public GameObject pausePanel;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenuToggle();
+        }
+    }
+
+    public void PauseMenuToggle()
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerInventoryScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventoryScript : MonoBehaviour
+{
+    public static PlayerInventoryScript instance { get; private set; }
+
+    public bool hasHoney = false;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
