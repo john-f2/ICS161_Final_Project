feb3ed84425c827bcedad5ae58add5fa5c7e0f83
JosephC1231 <Superjoe1231@gmail.com>
2019-02-23 20:48:39 -0800

    Fixes
    Fixed and change SeasonChangeSpriteScipt and copy pasted it to my own BearMazeScript that does the exact same thing and deleted the code I orginally put into the SeasonChangeSpriteScript.

--- Assets/Scripts/BearMazeChanges.cs
+++ Assets/Scripts/BearMazeChanges.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class SeasonChangeSpriteScript : MonoBehaviour
+public class BearMazeChanges : MonoBehaviour
 {
     [SerializeField] protected Sprite winter_sprite;
     [SerializeField] protected Sprite summer_sprite;
+    [SerializeField] protected bool winterwall;
     private SpriteRenderer m_spriterenderer;
+    private BoxCollider2D m_wall;
 
     void Awake()
     {
         m_spriterenderer = this.GetComponent<SpriteRenderer>();
+        m_wall = this.GetComponent<BoxCollider2D>();
     }
     // Start is called before the first frame update
     void Start()
@@ -22,17 +25,31 @@ public class SeasonChangeSpriteScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnSeasonChangeListener()
     {
         if (LevelManagerScript.instance.isWinter)
         {
+            if (m_wall)
+            {
+                if (winterwall)
+                    m_wall.enabled = true;
+                else
+                    m_wall.enabled = false;
+            }
             m_spriterenderer.sprite = winter_sprite;
         }
         else
         {
+            if (m_wall)
+            {
+                if (winterwall)
+                    m_wall.enabled = false;
+                else
+                    m_wall.enabled = true;
+            }
             m_spriterenderer.sprite = summer_sprite;
         }
     }
