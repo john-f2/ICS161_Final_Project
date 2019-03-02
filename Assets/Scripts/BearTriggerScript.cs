c7b41015d0d4c9c4b55ec4104d0b4f7ca6f55169
arianazuko <ashansen@uci.edu>
2019-03-01 21:59:54 -0800

    Made Bear Prefab and added to Level Manager in my Scene

---
+++ Assets/Scripts/BearTriggerScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTriggerScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            DialougeTypeScript.instance.WriteText("The bear is blocking the cave...but I shouldn't wake him up.");
+        }
+    }
+
+}
