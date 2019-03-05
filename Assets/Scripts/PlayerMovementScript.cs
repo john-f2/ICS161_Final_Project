2e1f5facbdea8aae65299e4f40edb180a9d97b29
SpacePlague <wtaraboc@uci.edu>
2019-03-05 03:12:52 -0800

    Boulder and Vine Solution added
    Vine sprites might need some adjustment.

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -4,31 +4,48 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    private Rigidbody2D player_rigidbody;
+	private Rigidbody2D player_rigidbody;
 
-    [SerializeField] protected float speed = 5;
-    void Awake()
-    {
-        player_rigidbody = this.GetComponent<Rigidbody2D>();
+	[SerializeField] protected float speed = 5;
+	[SerializeField] protected float slowspeed_modifier = 2;
+	void Awake()
+	{
+		player_rigidbody = this.GetComponent<Rigidbody2D>();
 
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
+	}
 
-    void Move()
-    {
-        float x_movement = Input.GetAxisRaw("Horizontal");
-        float y_movement = Input.GetAxisRaw("Vertical");
+	// Update is called once per frame
+	void Update()
+	{
+		Move();
+	}
 
-        player_rigidbody.velocity = new Vector2(x_movement * speed, y_movement * speed);
-    }
+	void Move()
+	{
+		float x_movement = Input.GetAxisRaw("Horizontal");
+		float y_movement = Input.GetAxisRaw("Vertical");
+
+		player_rigidbody.velocity = new Vector2(x_movement * speed, y_movement * speed);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Boulder"))
+		{
+			speed -= slowspeed_modifier;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Boulder"))
+		{
+			speed += slowspeed_modifier;
+		}
+	}
 }
---
+++ Assets/Scripts/VineScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VineScript : MonoBehaviour
+{
+	[SerializeField] protected Sprite vineBoulder;
+	[SerializeField] protected Sprite winter_sprite;
+	[SerializeField] protected Sprite summer_sprite;
+	private bool boulderCheck;
+	private SpriteRenderer m_spriterenderer;
+	private GameObject boulder;
+
+	void Awake()
+	{
+		m_spriterenderer = this.GetComponent<SpriteRenderer>();
+		boulderCheck = false;
+	}
+	// Start is called before the first frame update
+	void Start()
+	{
+		SeasonChangeButtonScript.instance.OnSeasonChange.AddListener(OnSeasonChangeListener);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnSeasonChangeListener()
+	{
+		if (boulderCheck)
+		{
+			if (!LevelManagerScript.instance.isWinter)
+			{
+				m_spriterenderer.sprite = vineBoulder;
+				Destroy(boulder);
+				this.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().enabled = false;
+			}
+		}
+		else
+		{
+			if (LevelManagerScript.instance.isWinter)
+			{
+				m_spriterenderer.sprite = winter_sprite;
+			}
+			else
+			{
+				m_spriterenderer.sprite = summer_sprite;
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Boulder"))
+		{
+			boulderCheck = true;
+			boulder = collision.gameObject;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (m_spriterenderer.sprite != vineBoulder && collision.gameObject.CompareTag("Boulder"))
+		{
+			boulderCheck = false;
+		}
+	}
+}
