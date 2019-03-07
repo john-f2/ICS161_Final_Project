2215e6719545d4b7628a7c8a97efe2de047badad
arianazuko <ashansen@uci.edu>
2019-03-06 16:54:21 -0800

    Made StonePathPrefabs, and StartMenu
    The Start Menu currently loads my scene but we can change it later.

---
+++ Assets/Scripts/StartMenuScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuScript : MonoBehaviour
+{
+    [SerializeField] protected GameObject controlsPanel;
+
+    public bool controlsUp = false;
+
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Ariana");
+    }
+
+    public void OnControlsButtonClicked()
+    {
+        controlsUp = !controlsUp;
+        if (controlsUp)
+        {
+            controlsPanel.SetActive(true);
+        }
+        else
+        {
+            controlsPanel.SetActive(false);
+        }
+    }
+
+    public void OnQuitButtonClicked()
+    {
+        Application.Quit();
+    }
+
+}
